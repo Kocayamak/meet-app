f8317db11cf1aa57489f4509b2c16239a339b215
Kocayamak <mikailkocayamak@gmail.com>
2024-05-14 21:26:33 +0300

    file structed created

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Meetings = ({params} : {params : { id : string }}) => {
+  return (
+    <div>Meeting Room # {params.id}</div>
+  )
+}
+
+export default Meetings
---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarLinks  = [
+    {
+        imgUrl : '/icons/Home.svg',
+        route : '/',
+        label : 'Anasayfa'
+    }, 
+    {
+        imgUrl : '/icons/upcoming.svg',
+        route : '/upcoming',
+        label : 'Yaklaşan'
+    },
+    {
+        imgUrl : '/icons/previous.svg',
+        route : '/previous',
+        label : 'Geçmiş'
+    },
+    {
+        imgUrl : '/icons/Video.svg',
+        route : '/recordings',
+        label : 'Kaydedilen'
+    },
+    {
+        imgUrl : '/icons/add-personal.svg',
+        route : '/personal-room',
+        label : 'Kişisel Odam'
+    }
+    
+]
